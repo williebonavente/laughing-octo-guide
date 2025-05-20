03616c0adf79c986959e2d14b4f09f02b35c7c2a
williebonavente <williebonavente@gmai.com>
2025-05-20 21:58:12 +0800

    Remove unecessary files and dirs

---
+++ Polytype/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Polytype.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(32, MinimumLength = 3)]
+        public string Username { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        public string PasswordHash { get; set; } = string.Empty;
+
+        // TODO: Forogot password must be included here too
+    }
+}
